aa98072d90273caa3c99b919bad99f6d6c1fa5e9
Adam Koller <adamkoller.86@gmail.com>
2022-01-27 22:29:09 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>A Simple React App</h1>
+        <button className="btn">Let's get started</button>
       </header>
     </div>
   );
