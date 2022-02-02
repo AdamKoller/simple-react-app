c3e19ab2c8ef5d7d9da8dc4af64933da6a815551
Adam Koller <adamkoller.86@gmail.com>
2022-02-02 14:03:41 -0600

    init

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses = [
-    { id: 1,
+    {
+      id: 1,
       title: 'Candy',
-      cost: 67.84, 
+      cost: 67.84,
       date: new Date(2021, 11, 31)
     },
-    { id: 2,
-      title: 'Table', 
-      cost: 298.32, 
+    {
+      id: 2,
+      title: 'Table',
+      cost: 298.32,
       date: new Date(2021, 12, 25)
     },
-    { id:3, 
-      title: 'Xmas Presents', 
-      cost: 402.98, 
+    {
+      id: 3,
+      title: 'Xmas Presents',
+      cost: 402.98,
       date: new Date(2021, 13, 22)
     },
-    { id:4, 
-      title: 'Near Years Eve Decorations', 
-      cost: 35.20, 
+    {
+      id: 4,
+      title: 'Near Years Eve Decorations',
+      cost: 35.29,
       date: new Date(2021, 13, 31)
     }
   ]
@@ -29,23 +33,23 @@ function App() {
     <div className="container">
       <header className="App-header">
         <h1>A Simple React App</h1>
-        <ExpenseItem 
-          title={expenses[0].title} 
+        <ExpenseItem
+          title={expenses[0].title}
           cost={expenses[0].cost}
           date={expenses[0].date}
         />
-        <ExpenseItem 
-          title={expenses[1].title} 
+        <ExpenseItem
+          title={expenses[1].title}
           cost={expenses[1].cost}
           date={expenses[1].date}
         />
-        <ExpenseItem 
-          title={expenses[2].title} 
+        <ExpenseItem
+          title={expenses[2].title}
           cost={expenses[2].cost}
           date={expenses[2].date}
         />
-        <ExpenseItem 
-          title={expenses[3].title} 
+        <ExpenseItem
+          title={expenses[3].title}
           cost={expenses[3].cost}
           date={expenses[3].date}
         />
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="item">
-        <h3>{props.date.toISOString()}</h3>
-        <div className="title"><h2>{props.title}</h2></div>
-        <div className="cost"><h3>${props.cost}</h3></div>
+      <div className="date">
+        <div className="month">{month}</div>
+        <div className="year">{year}</div>
+        <div className="day">{day}</div>
+      </div>
+      <div className="title-cost-wrapper">
+        <div className="title"><h3>{props.title}</h3></div>
+        <div className="cost"><h4>${props.cost}</h4></div>
+      </div>
     </div>
   );
 }
