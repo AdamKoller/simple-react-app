560c70cfcb054d861419e47c85fac410d0ef568d
Adam Koller <adamkoller.86@gmail.com>
2022-02-01 21:27:04 -0600

    init

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 11, 31);
+  const expenseTitle = 'Candy';
+  const expenseCost = 67.84;
+
   return (
     <div className="item">
-        <h3>Date</h3>
-        <div className="title"><h2>Title</h2></div>
-        <div className="cost"><h3>Amount</h3></div>
+        <h3>{expenseDate.toISOString()}</h3>
+        <div className="title"><h2>{expenseTitle}</h2></div>
+        <div className="cost"><h3>${expenseCost}</h3></div>
     </div>
   );
 }
