a2dc3d3bf72c7519e2105e97718c3d859d0c8cd1
Adam Koller <adamkoller.86@gmail.com>
2022-02-03 20:52:50 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
   const expenses = [
+    // Data to be rendered by components (ie; title, cost, and date of the 'expenses')
     {
       id: 1,
       title: 'Candy',
       cost: 67.84,
-      date: new Date(2021, 11, 31)
+      date: new Date(2021, 9, 31)
     },
     {
       id: 2,
       title: 'Table',
       cost: 298.32,
-      date: new Date(2021, 12, 25)
+      date: new Date(2021, 10, 25)
     },
     {
       id: 3,
       title: 'Xmas Presents',
       cost: 402.98,
-      date: new Date(2021, 13, 22)
+      date: new Date(2021, 11, 22)
     },
     {
       id: 4,
       title: 'Near Years Eve Decorations',
       cost: 35.29,
-      date: new Date(2021, 13, 31)
+      date: new Date(2021, 11, 31)
     }
   ]
 
@@ -33,26 +34,8 @@ function App() {
     <div className="container">
       <header className="App-header">
         <h1>A Simple React App</h1>
-        <ExpenseItem
-          title={expenses[0].title}
-          cost={expenses[0].cost}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          cost={expenses[1].cost}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          cost={expenses[2].cost}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          cost={expenses[3].cost}
-          date={expenses[3].date}
-        />
+        {/* the 'items' keyword will be replaced by the array 'expenses' */}
+        <ExpenseList items={expenses} />
       </header>
     </div>
   );
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import './ExpenseItem.css';
+import ExpenseItem from "./ExpenseItem";
 
-const ExpenseList = () => {
-    return <div>Q1WS</div>;
+const ExpenseList = (props) => {
+    return (
+        <div>
+            <ExpenseItem
+                title={props.items[0].title}
+                cost={props.items[0].cost}
+                date={props.items[0].date}
+            />
+            <ExpenseItem
+                title={props.items[1].title}
+                cost={props.items[1].cost}
+                date={props.items[1].date}
+            />
+            <ExpenseItem
+                title={props.items[2].title}
+                cost={props.items[2].cost}
+                date={props.items[2].date}
+            />
+            <ExpenseItem
+                title={props.items[3].title}
+                cost={props.items[3].cost}
+                date={props.items[3].date}
+            />
+        </div>
+    )
 };
 
 export default ExpenseList;
