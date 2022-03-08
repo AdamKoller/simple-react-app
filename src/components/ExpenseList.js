94ef93701ceb1ce10dd9557378a02335bd7d2dd1
Adam Koller <adamkoller.86@gmail.com>
2022-03-07 21:47:04 -0600

    init

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,31 +1,35 @@
 import './ExpenseItem.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from './Card';
+import ExpensesFilter from './NewExpense/ExpenseFilter';
 
 const ExpenseList = (props) => {
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                cost={props.items[0].cost}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                cost={props.items[1].cost}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                cost={props.items[2].cost}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                cost={props.items[3].cost}
-                date={props.items[3].date}
-            />
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter />
+                <ExpenseItem
+                    title={props.items[0].title}
+                    cost={props.items[0].cost}
+                    date={props.items[0].date}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    cost={props.items[1].cost}
+                    date={props.items[1].date}
+                />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    cost={props.items[2].cost}
+                    date={props.items[2].date}
+                />
+                <ExpenseItem
+                    title={props.items[3].title}
+                    cost={props.items[3].cost}
+                    date={props.items[3].date}
+                />
+            </Card>
+        </div>
     )
 };
 
